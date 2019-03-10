967c325c56a39ef687f07e1e26dc1d8ccd56c044
The Original G <33421040+OguzDastan@users.noreply.github.com>
2019-03-11 00:54:07 +0100

    hotelcontroller

--- RestServiceDatabase/DBUtil/ManageGuest.cs
+++ RestServiceDatabase/DBUtil/ManageGuest.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
 using ClassLibrary_Hotel;
 
 namespace RestServiceDatabase.DBUtil
@@ -10,12 +8,12 @@ namespace RestServiceDatabase.DBUtil
     public class ManageGuest : IManageGuest
     {
         private string queryString;
-        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HotelDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private string connectionString = "Data Source=oguzserverdb.database.windows.net;Initial Catalog=MyServerDB;User ID=oguz0040;Password=Dastan12;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         public List<Guest> GetAllGuest()
         {
             List<Guest> lg = new List<Guest>();
-            string queryString1 = "SELECT * FROM DemoGuest";
+            string queryString1 = "SELECT * FROM Guest";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -42,8 +40,8 @@ namespace RestServiceDatabase.DBUtil
         public Guest GetGuestFromId(int guestNr)
         {
             Guest g = new Guest();
-            queryString = $"SELECT * FROM DemoGuest " +
-                          $"WHERE Guest_No = {guestNr}";
+            queryString = $"SELECT * FROM Guest " +
+                          $"WHERE Id = {guestNr}";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 SqlCommand command = new SqlCommand(queryString, connection);
@@ -53,17 +51,17 @@ namespace RestServiceDatabase.DBUtil
                 while (reader.Read())
                 {
                     g.GuestNo = reader.GetInt32(0);
+                    g.Name = reader.GetString(1);
+                    g.Adress = reader.GetString(2);
                 }
-                reader.Close();
-                connection.Close();
             }
             return g;
         }
 
         public bool CreateGuest(Guest guest)
         {
-            queryString = $"INSERT INTO DemoGuest " +
-                          $"(Guest_No, Name, Address) " +
+            queryString = $"INSERT INTO Guest " +
+                          $"(Id, Name, Address) " +
                           $"VALUES ({guest.GuestNo}, '{guest.Name}', '{guest.Adress}') ";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -75,21 +73,19 @@ namespace RestServiceDatabase.DBUtil
 
                 if (i > 0)
                 {
+                    Console.WriteLine("Success!");
                     return true;
                 }
-
-                command.Clone();
-                connection.Close();
             }
             return false;
         }
 
         public bool UpdateGuest(Guest guest, int guestNr)
         {
-            queryString = $"UPDATE DemoGuest SET " +
+            queryString = $"UPDATE Guest SET " +
                           $"Name = '{guest.Name}', " +
                           $"Address = '{guest.Adress}' " +
-                          $"WHERE Guest_No = {guestNr}";
+                          $"WHERE Id = {guestNr}";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -102,8 +98,6 @@ namespace RestServiceDatabase.DBUtil
                 {
                     return true;
                 }
-                command.Clone();
-                connection.Close();
             }
             return false;
         }
@@ -111,17 +105,38 @@ namespace RestServiceDatabase.DBUtil
         public Guest DeleteGuest(int guestNr)
         {
             Guest g = new Guest();
-            
-            queryString = $"DELETE FROM DemoGuest " +
-                          $"WHERE Guest_No = {guestNr}";
+
+            // Read soon-to-be-deleted data
+            string queryString2 = "SELECT * FROM Guest " +
+                                  $"WHERE Id = {guestNr}";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                SqlCommand command = new SqlCommand(queryString, connection);
+                SqlCommand command = new SqlCommand(queryString2, connection);
+                command.Connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    g.GuestNo = reader.GetInt32(0);
+                    g.Name = reader.GetString(1);
+                    g.Adress = reader.GetString(2);
+                }
+            }
+
+            // Delete data
+            string queryString1 = $"DELETE FROM Guest " +
+                          $"WHERE Id = {guestNr}";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString1, connection);
                 command.Connection.Open();
 
                 command.ExecuteNonQuery();
             }
+
             return g;
         }
     }
